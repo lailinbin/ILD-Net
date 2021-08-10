b5166e7846d77a3e32430bd4d9e5a96c55ba98a6
LinbinLai <429220579@qq.com>
2021-08-10 20:40:59 +0800

    version:1.0.1 description:add file data_statistics.py,data_preprocessor.py

---
+++ ReadMe.md
@@ -0,0 +1,33 @@
+# Deep Learning for Auxiliary Diagnostic System of lInterstitial Lung Disease------ILDNet
+
+[TOC]
+
+## Background
+
+## Install
+### 1.Create virtual environment by Anaconda
+```shell
+conda create -n env_name python==3.6
+```
+### 2.Activate conda environment
+```shell
+conda activate env_name
+```
+### 3.Install related libraries
+```shell
+pip install -r requirements.txt
+```
+## Usage
+### Step1.Data statistics
+This step is for dataset statistics information calculation and save. For analysing your own dataset, you can use the following statement:
+```shell
+python .\code\data_statistics.py -i dataset_path -o excel_path
+```
+You can view [Dataset Information](##Dataset Information) for detail of our dataset.
+### Step2.Data preprocess
+
+## Dataset Information
+## Maintainers
+| Name | E-mail |Address|
+| :----: | :----: | :----:|
+|Linbin Lai|lailinbin@mail.nwpu.edu.cn| 127 West Youyi Road, Beilin District, Xi'an Shaanxi, 710072, P.R.China.|
---
+++ code/data_statistics.py
@@ -0,0 +1,37 @@
+# -*- coding: UTF-8 -*-
+'''
+#Data: 2021/8/10 20:00
+#FilePath: data_statistics.py
+#Author: LaiLinbin
+#CopyRight: AI Lab 2021
+#Description: This file is used to calculate the statistics information of dataset
+'''
+# import lib here
+from data import Data_statistics
+import argparse
+def get_parser():
+    # 实例化ArgumentParser
+    parser = argparse.ArgumentParser(description='please input the path of dataset')
+    # 添加参数设置
+    parser.add_argument('-i',
+                        type=str,
+                        dest='dataset_path',
+                        default='.././3Dimg_data',
+                        help='path of dataset for statistics analysis')
+
+    parser.add_argument('-o',
+                        type=str,
+                        dest='excel_path',
+                        default='.././experiment/statistics.xlsx',
+                        help='path of excel file to save the statistics information')
+
+    args = parser.parse_args()
+    return  args
+
+if __name__ == '__main__':
+    args = get_parser()
+    data_statistics = Data_statistics(args.dataset_path)
+    print('统计样本信息中')
+    data_statistics.save_statistics_information(args.excel_path)
+    print('样本信息统计完成')
+    pass
